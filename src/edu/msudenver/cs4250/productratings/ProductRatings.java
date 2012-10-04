9792d9e675c7289774a7f4eac9079c9dcf1ea91d
Mrinal Shukla <mrinal.shukla.007@gmail.com>
2012-10-03 18:36:59 -0600

    initial commit & sync with a java project skeleton

---
+++ src/edu/msudenver/cs4250/productratings/ProductRatings.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package edu.msudenver.cs4250.productratings;
+
+/**
+ * @author Mrinal
+ *
+ */
+public class ProductRatings {
+
+	/**
+	 * 
+	 */
+	public ProductRatings() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/edu/msudenver/cs4250/productratings/ProductRatingsTest.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package edu.msudenver.cs4250.productratings;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Mrinal
+ *
+ */
+public class ProductRatingsTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
